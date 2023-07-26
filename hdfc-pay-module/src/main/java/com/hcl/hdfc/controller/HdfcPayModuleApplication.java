08a1c2081eaddaf1c2184c776d498703047eb675
malle <malle@host.docker.internal>
2023-07-26 15:49:09 +0530

    jkjnksd

---
+++ hdfc-pay-module/src/main/java/com/hcl/hdfc/controller/HdfcPayModuleApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.hdfc.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdfcPayModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdfcPayModuleApplication.class, args);
+		System.out.println("welocme hdfc payments");
+	}
+
+}
---
+++ hdfc-pay-module/src/test/java/com/hcl/hdfc/controller/HdfcPayModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.hdfc.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdfcPayModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
